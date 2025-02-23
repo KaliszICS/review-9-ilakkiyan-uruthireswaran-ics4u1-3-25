a875d016c30f32d3300838e40083765097d7160b
ilakkiyan-uruthireswaran-ics4u1-3-25 <Ilakkiyan.uruthireswaran@student.tdsb.on.ca>
2025-02-23 20:28:05 +0000

    Passed every test

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,36 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static int lettersToFive(String str, char letter) {
+		int word = 0;
+		int word2 = 0;
+		while (word2 < str.length()) {
+			if (str.charAt(word2) == letter){
+				word++;
+				if (word == 5) {
+					return word2 + 1;
+				}
+			}
+			word2++;
+		} 
+		return -1;
+	}
 
+	public static int countLetter(String var, char ltr) {
+		int count = 0;
+		for (int i = 0; i < var.length(); i++) {
+			if (var.charAt(1) == ltr) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String args[]) {
+		int result = lettersToFive("Hope you all have a great day", 'a');
+		System.out.println(result);
+
+		int result2 = countLetter("hello", 'l');
+		System.out.println(result2);
 	}
 
 }
